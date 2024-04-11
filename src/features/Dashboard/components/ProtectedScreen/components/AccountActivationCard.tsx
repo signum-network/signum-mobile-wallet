c82d67586039ff4334fa158b78a98c6b8fd79760
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2024-04-11 13:29:25 -0400

    Multiple updates
    - Initial account activation feature implementation
    - Update on useAccountStore hook

--- src/features/Dashboard/components/ProtectedScreen/components/AccountActivationCard.tsx
+++ src/features/Dashboard/components/ProtectedScreen/components/AccountActivationCard.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 import { View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useNodeHostStore } from "@/hooks/useNodeHostStore";
+import { useLedgerService } from "@/hooks/useLedgerService";
+import { useAccount } from "@/hooks/useAccount";
 import { Text } from "@/components/Text";
 import { Card } from "@/components/Card";
 import { Button } from "@/components/Button";
+
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export const AccountActivationCard = () => {
   const { t } = useTranslation();
-  const { isTestnet } = useNodeHostStore();
+  const { ledgerService } = useLedgerService();
+  const { accountId, publicKey } = useAccount();
 
   const [activationPending, setActivationPending] = useState(false);
 
-  const requestActivation = async () => {};
+  const requestActivation = async () => {
+    if (!ledgerService) return;
+
+    await ledgerService.account
+      .activate(accountId, publicKey)
+      .then(() => alert(t("unsafeAccount.accountActivationIsPending")))
+      .catch(() => alert(t("unsafeAccount.accountActivationIsPending")))
+      .finally(() => setActivationPending(true));
+  };
 
   return (
     <View className="flex-1 flex flex-col items-center justify-center w-full max-w-sm mx-auto">
@@ -32,9 +43,14 @@ export const AccountActivationCard = () => {
           <Button
             icon={<Ionicons name="lock-closed" size={24} color="white" />}
             type="primary"
-            title={t("unsafeAccount.activate")}
+            title={t(
+              activationPending
+                ? "unsafeAccount.activating"
+                : "unsafeAccount.activate"
+            )}
             wide
             extraClassNames="mt-4"
+            disabled={activationPending}
             pressableProps={{ onPress: requestActivation }}
           />
         </View>
--- src/hooks/useAccountStore.ts
+++ src/hooks/useAccountStore.ts
@@ -1,7 +1,8 @@
 import { accountStore } from "@/states/accountStore";
+import { useShallow } from "zustand/react/shallow";
 
 export const useAccountStore = () => {
-  const accounts = accountStore((state) => state.accounts);
+  const accounts = accountStore(useShallow((state) => state.accounts));
   const addAccount = accountStore((state) => state.addAccount);
   const deleteAccount = accountStore((state) => state.deleteAccount);
 
